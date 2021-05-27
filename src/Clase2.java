4cd83978e67b6a9173ba7bc41a78ca27e4b1f803
Maria <mariagarciavidal@cdpjosecabrera.es>
2021-05-27 12:20:49 +0200

    Comic 1

--- src/Clase2.java
+++ src/Clase2.java
@@ -3,7 +3,9 @@ public class Clase2 {
 
 	public Clase2() {
 		// TODO Apéndice de constructor generado automáticamente
-		
+		int multi, num3=6, num4=5;
+		multi=num3*num4;
+		System.out.println(multi);
 		
 	}
 
